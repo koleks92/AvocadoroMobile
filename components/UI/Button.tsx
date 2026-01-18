5d0cfc4d8bf48b825e5e1638fae29710ca7dec03
Jan Konieczek <koleks92@gmail.com>
2026-01-18 15:07:35 +0100

    feat: Button styles v1

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,3 +1,6 @@
+import { Colors } from "@/constants/Colors";
+import { Sizes } from "@/constants/Sizes";
+import { textDefault } from "@/constants/Styles";
 import { Pressable, StyleSheet, Text } from "react-native";
 
 interface ButtonProps {
@@ -5,27 +8,33 @@ interface ButtonProps {
     onPress: () => void;
 }
 
-export default function Button({ title, onPress}: ButtonProps) {
+export default function Button({ title, onPress }: ButtonProps) {
     return (
         <Pressable
-        onPress={onPress}
-        style={({ pressed }) => [
-            styles.root, pressed ? styles.pressed : null
-        ]}
+            onPress={onPress}
+            style={({ pressed }) => [
+                styles.root,
+                pressed ? styles.pressed : null,
+            ]}
         >
             <Text style={styles.title}>{title}</Text>
         </Pressable>
-    )
-};
+    );
+}
 
 const styles = StyleSheet.create({
     root: {
-
+        backgroundColor: Colors.primaryGreen,
+        borderRadius: Sizes.buttonBorderRadius,
+        padding: Sizes.buttonPadding,
+        margin: Sizes.buttonMargin,
     },
     pressed: {
-
+        backgroundColor: Colors.primaryDarkGreen,
+        transform: [{ scale: 0.99 }],
     },
     title: {
-
-    }
-})
+        ...textDefault,
+        fontSize: Sizes.buttonFont,
+    },
+});
--- constants/Sizes.tsx
+++ constants/Sizes.tsx
@@ -1,5 +1,14 @@
-import {Dimensions} from 'react-native';
+import { Dimensions } from "react-native";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
+// Change depending on the screen
+const windowSize = windowWidth > windowHeight ? windowHeight: windowWidth;
+
+export const Sizes = {
+    buttonFont: windowSize * 0.06,
+    buttonPadding: windowSize * 0.04,
+    buttonBorderRadius: windowSize * 0.10,
+    buttonMargin: windowSize * 0.01
+};
