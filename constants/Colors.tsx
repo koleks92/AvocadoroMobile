d5db9e73f71aa4f6b257fc4ecef2fa7dd7f96868
Jan Konieczek <koleks92@gmail.com>
2026-01-18 13:57:23 +0100

    feat: Added Colors from Avocadoro

---
+++ constants/Colors.tsx
@@ -0,0 +1,10 @@
+export const Colors = {
+    primaryGreen: "#6CBF4F",
+    primaryDarkGreen: "#4F9A39",
+    accentGreenLight: "#A7E68A",
+    accentGreenDark: "#2F7A24",
+    gray900: "#0F0F0F",
+    gray800: "#1A1A1A",
+    gray700: "#2B2B2B",
+    white: "#FAFAF8",
+} as const;
