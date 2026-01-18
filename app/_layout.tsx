4bd1072ec177d340390954861ae780c857b03048
Jan Konieczek <koleks92@gmail.com>
2026-01-18 13:38:42 +0100

    feat: headerShown to false

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,24 @@
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/Montserrat-VariableFont_wght.ttf'),
-  });
+    const [loaded] = useFonts({
+        SpaceMono: require("../assets/fonts/Montserrat-VariableFont_wght.ttf"),
+    });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hide();
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hide();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return <Stack />;
+    return <Stack screenOptions={{ headerShown: false }} />;
 }
