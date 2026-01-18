64ac2f310dcc2a208b22534dc1d48da17860efd5
Jan Konieczek <koleks92@gmail.com>
2026-01-18 14:01:11 +0100

    fix: Changed names to be more consistent with Avocadoro

--- constants/Colors.tsx
+++ constants/Colors.tsx
@@ -3,8 +3,8 @@ export const Colors = {
     primaryDarkGreen: "#4F9A39",
     accentGreenLight: "#A7E68A",
     accentGreenDark: "#2F7A24",
-    gray900: "#0F0F0F",
-    gray800: "#1A1A1A",
-    gray700: "#2B2B2B",
+    background1: "#0F0F0F",
+    background2: "#1A1A1A",
+    background3: "#2B2B2B",
     white: "#FAFAF8",
 } as const;
