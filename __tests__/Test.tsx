150e3000759ac5185098beba7d7543020fde3a35
Jan Konieczek <koleks92@gmail.com>
2026-01-19 14:02:44 +0100

    test: Changes tests for index

--- __tests__/Test.tsx
+++ __tests__/Test.tsx
@@ -2,10 +2,10 @@ import { render } from '@testing-library/react-native';
 
 import Index from '@/app/index';
 
-describe('Index', () => {
+describe('Index & Log In', () => {
     test("Renders text correctly", () => {
         const { getByText } = render(<Index />);
 
-        getByText("Edit app/index.tsx to edit this screen.");
+        getByText("Log In");
     })
 })
