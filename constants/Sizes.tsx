3c5ababf27f2291ee933cd3627197f50be98ad6f
Jan Konieczek <koleks92@gmail.com>
2026-01-19 14:01:28 +0100

    feat: Input Sizes

--- constants/Sizes.tsx
+++ constants/Sizes.tsx
@@ -4,13 +4,21 @@ const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 // Change depending on the screen
-const windowSize = windowWidth > windowHeight ? windowHeight: windowWidth;
+const windowSize = windowWidth > windowHeight ? windowHeight : windowWidth;
 
 export const Sizes = {
+    // Button
     buttonFont: windowSize * 0.06,
     buttonIcon: windowSize * 0.08,
     buttonIconWidth: windowSize * 0.16,
     buttonPadding: windowSize * 0.04,
-    buttonBorderRadius: windowSize * 0.10,
-    buttonMargin: windowSize * 0.01
+    buttonBorderRadius: windowSize * 0.1,
+    buttonMargin: windowSize * 0.01,
+
+    // Input
+    inputFont: windowSize * 0.06,
+    inputPadding: windowSize * 0.04,
+    inputMargin: windowSize * 0.01,
+    inputBorderRadius: windowSize * 0.1,
+    inputWidth: windowSize * 0.6
 };
