b28e3d29c3990affa3d4b2828f80544cbb4cea16
Jan Konieczek <koleks92@gmail.com>
2026-01-19 20:27:07 +0100

    feat: Added rootStyles

--- constants/Styles.tsx
+++ constants/Styles.tsx
@@ -1,6 +1,17 @@
-import { Colors } from "./Colors"
+import { Colors } from "./Colors";
+import { Sizes } from "./Sizes";
 
 export const textDefault = {
-        color: Colors.white,
-        fontFamily: "MontserratSemiBold",
+    color: Colors.white,
+    fontFamily: "MontserratSemiBold",
+} as const;
+
+export const rootStyles = {
+    flex: 1,
+    backgroundColor: Colors.background2,
+    margin: Sizes.rootMargin,
+    padding: Sizes.rootPadding,
+    borderRadius: Sizes.rootMargin,
+    justifyContent: "center",
+    alignItems: "center",
 } as const;
