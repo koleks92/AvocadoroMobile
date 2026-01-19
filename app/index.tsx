45707bfe2604d1c6410abc37e6a19da564622197
Jan Konieczek <koleks92@gmail.com>
2026-01-19 19:22:51 +0100

    feat: Added logo animation, KeyboardAvoidingView

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
+import ShakingLogo from "@/components/UI/AvocadoroImage";
 import Button from "@/components/UI/Button";
+import InputField from "@/components/UI/Input";
 import { Sizes } from "@/constants/Sizes";
 import { textDefault } from "@/constants/Styles";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useRouter } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+    Image,
+    Keyboard,
+    KeyboardAvoidingView,
+    Platform,
+    StyleSheet,
+    TouchableWithoutFeedback,
+    View,
+} from "react-native";
 
 export default function Index() {
     const router = useRouter();
+
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
     const googleSignIn = async (): Promise<void> => {
         console.log("TODO");
@@ -17,28 +31,61 @@ export default function Index() {
     };
 
     return (
-        <View style={styles.root}>
-            <Text style={styles.text}>Login</Text>
-            <View>
-                <Button
-                    title="Log In"
-                    onPress={() => {
-                        router.navigate("/dashboard");
-                    }}
-                />
-            </View>
-            <View style={styles.socialView}>
-                <Button
-                    title={
-                        <FontAwesome name="google" size={Sizes.buttonIcon}/>
-                    }
-                    onPress={() => googleSignIn()} icon={true}
-                />
-                <Button title={
-                        <FontAwesome name="apple" size={Sizes.buttonIcon}/>
-                    } onPress={() => appleSignIn()} icon={true}/>
-            </View>
-        </View>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1 }}
+        >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.root}>
+                    <View style={styles.logoInputView}>
+                        <ShakingLogo />
+                        <InputField
+                            placeholder="Enter your email"
+                            value={email}
+                            onChangeText={(val) => setEmail(val)}
+                            inputMode="email"
+                        />
+                        <InputField
+                            placeholder="Enter your password"
+                            value={password}
+                            onChangeText={(val) => setPassword(val)}
+                            inputMode="text"
+                            password={true}
+                        />
+                    </View>
+                    <View style={styles.buttonsView}>
+                        <Button
+                            title="Log In"
+                            onPress={() => {
+                                router.navigate("/dashboard");
+                            }}
+                        />
+                        <View style={styles.socialView}>
+                            <Button
+                                title={
+                                    <FontAwesome
+                                        name="google"
+                                        size={Sizes.buttonIcon}
+                                    />
+                                }
+                                onPress={() => googleSignIn()}
+                                icon={true}
+                            />
+                            <Button
+                                title={
+                                    <FontAwesome
+                                        name="apple"
+                                        size={Sizes.buttonIcon}
+                                    />
+                                }
+                                onPress={() => appleSignIn()}
+                                icon={true}
+                            />
+                        </View>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     );
 }
 
@@ -51,8 +98,20 @@ const styles = StyleSheet.create({
     text: {
         ...textDefault,
     },
+    logoInputView: {
+        display: "flex",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttonsView: {
+        display: "flex",
+    },
     socialView: {
         display: "flex",
         flexDirection: "row",
     },
+    logo: {
+        height: Sizes.loginLogo,
+    }
 });
