59324cfd6de626c6a6d8cbc07a464e77e0e8dbdc
Jan Konieczek <koleks92@gmail.com>
2026-01-21 20:24:55 +0100

    feat: AvocadoroContext with supabase, session

---
+++ store/AvocadoroContext.tsx
@@ -0,0 +1,46 @@
+import { supabase } from "@/lib/supabase";
+import { Session, SupabaseClient } from "@supabase/supabase-js";
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+// Define the shape of the context value
+type AvocadoroContextType = {
+    supabase: SupabaseClient;
+    session: Session | null;
+    setSession: React.Dispatch<React.SetStateAction<Session | null>>;
+};
+
+// Create the context (default: null so we can handle initialization)
+export const AvocadoroContext = createContext<AvocadoroContextType | null>(
+    null,
+);
+
+// Define props type for the Provider
+type AvocadoroProviderProps = {
+    children: ReactNode;
+};
+
+// The provider itself
+export function AvocadoroProvider({ children }: AvocadoroProviderProps) {
+    const [session, setSession] = useState<Session | null>(null);
+
+
+    useEffect(() => {
+        supabase.auth
+            .getSession()
+            .then(({ data: { session } }) => setSession(session));
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange((_event, session) =>
+            setSession(session),
+        );
+        return () => subscription.unsubscribe();
+    }, []);
+
+    return (
+        <AvocadoroContext.Provider
+            value={{ supabase, session, setSession }}
+        >
+            {children}
+        </AvocadoroContext.Provider>
+    );
+}
