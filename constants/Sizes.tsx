46bd8c7863225fb6fb7f76ee9c0d86ce5ab1d7a7
Jan Konieczek <koleks92@gmail.com>
2026-02-04 20:54:15 +0100

    fix: Icon bigger

--- constants/Sizes.tsx
+++ constants/Sizes.tsx
@@ -13,7 +13,7 @@ export const Sizes = {
 
     // Button
     buttonFont: windowSize * 0.05,
-    buttonIcon: windowSize * 0.06,
+    buttonIcon: windowSize * 0.07,
     buttonIconWidth: windowSize * 0.2,
     buttonPadding: windowSize * 0.04,
     buttonBorderRadius: windowSize * 0.1,
