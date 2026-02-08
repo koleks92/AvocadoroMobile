132d96272f32665ef97d76449c4fabf9ee1e20a3
Jan Konieczek <koleks92@gmail.com>
2026-02-08 21:07:50 +0100

    feat: Added reading values from route

--- app/group.tsx
+++ app/group.tsx
@@ -1,11 +1,23 @@
 import AnimatedRoot from "@/components/UI/AnimatedRoot";
 import GoBackButton from "@/components/UI/GoBackButton";
 import { rootStyles, textDefault } from "@/constants/Styles";
-import { useRouter } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+
+type SessionGroupProps = {
+    id: string
+    name: string;
+    focusTimer: string;
+    breakTimer: string;
+    totalMinutes: string;
+}
 
 export default function Group() {
     const router = useRouter();
+
+    // Read the values from the route
+    const { name, focusTimer, breakTimer, totalMinutes, id } =
+        useLocalSearchParams<SessionGroupProps>();
 
     return (
         <>
