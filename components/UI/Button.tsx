0c6c4fa79c03aa52468e9bd0ef30c1cac829487f
Jan Konieczek <koleks92@gmail.com>
2026-02-11 12:45:39 +0100

    feat: Added timer, isSelected props

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,24 +1,35 @@
 import { Colors } from "@/constants/Colors";
 import { Sizes } from "@/constants/Sizes";
 import { textDefault } from "@/constants/Styles";
+import { ReactNode } from "react";
 import { Pressable, StyleSheet, Text } from "react-native";
-import { ReactNode } from 'react';
 
 interface ButtonProps {
     title: string | ReactNode;
     onPress: () => void;
     icon?: boolean;
+    timer?: boolean;
+    isSelected?: boolean;
     accessibilityLabel?: string;
 }
 
-export default function Button({ title, onPress, icon, accessibilityLabel }: ButtonProps) {
+export default function Button({
+    title,
+    onPress,
+    icon,
+    timer,
+    isSelected,
+    accessibilityLabel,
+}: ButtonProps) {
     return (
         <Pressable
             onPress={onPress}
             style={({ pressed }) => [
                 styles.root,
                 pressed ? styles.pressed : null,
-                icon ? styles.icon : null
+                icon ? styles.icon : null,
+                timer ? styles.timer : null,
+                isSelected ? styles.selected : null,
             ]}
             accessibilityLabel={accessibilityLabel}
         >
@@ -30,9 +41,9 @@ export default function Button({ title, onPress, icon, accessibilityLabel }: But
 const styles = StyleSheet.create({
     root: {
         display: "flex",
-        justifyContent: 'center',
-        alignItems: 'center',
-//      backgroundColor: Colors.primaryGreen,
+        justifyContent: "center",
+        alignItems: "center",
+        //      backgroundColor: Colors.primaryGreen,
         borderRadius: Sizes.buttonBorderRadius,
         padding: Sizes.buttonPadding,
         height: Sizes.buttonInputHeight,
@@ -41,15 +52,20 @@ const styles = StyleSheet.create({
     icon: {
         borderRadius: Sizes.smallButtonHeight / 2,
         width: Sizes.smallButtonHeight,
-        height: Sizes.smallButtonHeight
+        height: Sizes.smallButtonHeight,
+    },
+    timer: {
+        margin: 0,
     },
     pressed: {
         backgroundColor: Colors.primaryDarkGreen,
         transform: [{ scale: 0.99 }],
     },
+    selected: {
+        backgroundColor: Colors.primaryDarkGreen,
+    },
     title: {
         ...textDefault,
         fontSize: Sizes.buttonFont,
     },
-
 });
