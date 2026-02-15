ec6748b299e661b858df6aeecb5685141ab8581f
Jan Konieczek <koleks92@gmail.com>
2026-02-15 13:31:09 +0100

    feat: Settings screen finished v1

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,23 +1,88 @@
 import AnimatedRoot from "@/components/UI/AnimatedRoot";
 import Button from "@/components/UI/Button";
 import GoBackButton from "@/components/UI/GoBackButton";
+import { Sizes } from "@/constants/Sizes";
 import { rootStyles, textDefault } from "@/constants/Styles";
+import { useAvocadoro } from "@/store/AvocadoroContext";
 import { useRouter } from "expo-router";
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Settings() {
     const router = useRouter();
 
+    const [buttonWidth, setButtonWidth] = useState<number>(0);
+
+    const [deleteView, setDeleteView] = useState<boolean>(false);
+
+    const { supabase } = useAvocadoro();
+
+    async function deleteAccountHandler(): Promise<void> {
+        const { error: rpcError } = await supabase.rpc("delete_user");
+
+        if (rpcError) {
+            console.error("Error deleting account via RPC:", rpcError.message);
+            return;
+        }
+
+        await supabase.auth.signOut();
+        router.navigate("/");
+    }
+
     return (
         <>
             <AnimatedRoot />
             <View style={styles.root}>
-                <GoBackButton
-                    onPress={() => {
-                        router.back();
-                    }}
-                />
-                <Text style={styles.text}>Settings</Text>
+                <View style={styles.topView}>
+                    <View
+                        onLayout={(event) => {
+                            const { width } = event.nativeEvent.layout;
+                            setButtonWidth(width);
+                        }}
+                    >
+                        <GoBackButton
+                            onPress={() => {
+                                {
+                                    deleteView
+                                        ? setDeleteView(false)
+                                        : router.back();
+                                }
+                            }}
+                        />
+                    </View>
+                    <View style={styles.titleView}>
+                        <Text style={styles.titleText}>Settings</Text>
+                    </View>
+                    <View style={{ width: buttonWidth }} />
+                </View>
+                <View style={styles.middleView}>
+                    {deleteView ? (
+                        <>
+                            <Text style={styles.text}>
+                                We're sorry to see you go.
+                            </Text>
+                            <Text style={styles.text}>
+                                If you delete your account, you'll lose access
+                                to your progress and saved settings forever.
+                            </Text>
+                            <View style={styles.buttonView}>
+                                <Button
+                                    title="Remove account"
+                                    onPress={() => deleteAccountHandler()}
+                                    deleteButton={true}
+                                    accessibilityLabel="delete-button-confirm"
+                                />
+                            </View>
+                        </>
+                    ) : (
+                        <Button
+                            title="Remove account"
+                            onPress={() => setDeleteView(true)}
+                            deleteButton={true}
+                            accessibilityLabel="delete-button"
+                        />
+                    )}
+                </View>
             </View>
         </>
     );
@@ -25,7 +90,33 @@ export default function Settings() {
 
 const styles = StyleSheet.create({
     root: rootStyles,
+    topView: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: Sizes.topBottomHeight,
+        width: "100%",
+    },
+    titleView: {
+        justifyContent: "center",
+        alignItems: "center",
+        width: "50%",
+    },
+    titleText: {
+        ...textDefault,
+        textAlign: "center",
+        fontSize: Sizes.titleSize,
+    },
+    middleView: {
+        flex: 1,
+        padding: Sizes.settingsPadding,
+    },
     text: {
         ...textDefault,
+        textAlign: "center",
+        fontSize: Sizes.settingsText,
+        marginVertical: Sizes.settingsTextMargin,
+    },
+    buttonView: {
+        marginVertical: Sizes.settingsTextMargin,
     },
 });
