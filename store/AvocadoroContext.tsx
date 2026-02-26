02d29c4ff5c36e3e3d73eb692e5978768c24e1e1
Jan Konieczek <koleks92@gmail.com>
2026-02-26 21:25:20 +0100

    feat: Added message

--- store/AvocadoroContext.tsx
+++ store/AvocadoroContext.tsx
@@ -15,6 +15,8 @@ type AvocadoroContextType = {
     setSession: React.Dispatch<React.SetStateAction<Session | null>>;
     timerOn: boolean;
     setTimerOn: React.Dispatch<React.SetStateAction<boolean>>;
+    message: string;
+    setMessage: React.Dispatch<React.SetStateAction<string>>;
 };
 
 // Create the context (default: null so we can handle initialization)
@@ -39,6 +41,7 @@ type AvocadoroProviderProps = {
 export function AvocadoroProvider({ children }: AvocadoroProviderProps) {
     const [session, setSession] = useState<Session | null>(null);
     const [timerOn, setTimerOn] = useState<boolean>(false);
+    const [message, setMessage] = useState<string>("");
 
     useEffect(() => {
         supabase.auth
@@ -54,7 +57,7 @@ export function AvocadoroProvider({ children }: AvocadoroProviderProps) {
 
     return (
         <AvocadoroContext.Provider
-            value={{ supabase, session, setSession, timerOn, setTimerOn }}
+            value={{ supabase, session, setSession, timerOn, setTimerOn, message, setMessage }}
         >
             {children}
         </AvocadoroContext.Provider>
