111ae4c8376bebf9ec900d677f004ffc648c1025
Jan Konieczek <koleks92@gmail.com>
2026-03-07 13:26:26 +0100

    feat: Sentry added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,26 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://4510923ab15a8db2a79d0455db1b2f9e@o4509752951963648.ingest.de.sentry.io/4511003180007504',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 SplashScreen.preventAutoHideAsync();
 
@@ -13,7 +33,7 @@ SplashScreen.setOptions({
     fade: true,
 });
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
     const [loaded] = useFonts({
         MontserratRegular: require("../assets/fonts/Montserrat-Regular.ttf"),
         MontserratItalic: require("../assets/fonts/Montserrat-Italic.ttf"),
@@ -50,4 +70,4 @@ export default function RootLayout() {
             </SafeAreaView>
         </SafeAreaProvider>
     );
-}
+});
